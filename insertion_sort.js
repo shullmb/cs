a16eac53897d64103fb7bb56afc47261c3df62e6
shullmb <shullmb@gmail.com>
2018-07-30 19:16:23 -0700

    console logs

--- insertion_sort.js
+++ insertion_sort.js
@@ -1,23 +1,24 @@
-let arr = [5, 4, 3, 2, 1]
+let arr = [84, 69, 51, 75, 83, 12, 2, 1, 11, 30, 42, 1]
 
 // iterate over array
 // compare current value to previous value
 
 function insertionSort(arr) {
-	for (let i=0; i <= arr.length; i++) {
+	counters = {swaps: 0, comparisons: 0}
+	for (let i = 0; i <= arr.length; i++) {
 		for (let j = 0; j <= arr.length; j++) {
-			if (arr[j] <= arr[j - 1]) {
+			counters.comparisons += 1
+			if (arr[j] < arr[j - 1]) {
 				let lesser = arr[j]
 				let greater = arr[j - 1]
 				arr[j] = greater
 				arr[j - 1] = lesser
 				console.log('swap', arr[j], arr[j - 1], arr)
-			} else {
-				console.log('no swap')
+				counters.swaps += 1
 			}
 		}
 	}
-	console.log('final output', arr)
+	console.log('final output', arr, 'num swaps: ', counters.swaps, 'num comps: ', counters.comparisons)
 }
 
 insertionSort(arr);
