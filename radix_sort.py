c54600818c4bfb4b49057f74a8a2fbd4c440005b
shullmb <shullmb@gmail.com>
2018-08-01 05:29:52 -0700

    wiki solution -- not handling neg nums

--- radix_sort.py
+++ radix_sort.py
@@ -2,7 +2,33 @@ from random import shuffle
 
 data = [0,5,-3,100,1000,75,3,44,3490,23,4,435,123,49,403,-30,2498]
 
+def radix_sort(array, base=10):
+    def list_to_buckets(array, base, iteration):
+        buckets = [[] for x in range(base)]
+        for number in array:
+            # Isolate the base-digit from the number
+            digit = (number // (base ** iteration)) % base
+            # Drop the number into the correct bucket
+            buckets[digit].append(number)
+        return buckets
 
+    def buckets_to_list(buckets):
+        numbers = []
+        for bucket in buckets:
+            # append the numbers in a bucket
+            # sequentially to the returned array
+            for number in bucket:
+                numbers.append(number)
+        return numbers
 
-def radix_sort(arr):
-    pass
+    maxval = max(array)
+
+    it = 0
+    # Iterate, sorting the array by each base-digit
+    while base ** it <= maxval:
+        array = buckets_to_list(list_to_buckets(array, base, it))
+        it += 1
+
+    return array
+
+print(radix_sort(data))
