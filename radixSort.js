1b3ebac62e7e4ee543895d24df9323b4a71c25e2
shullmb <shullmb@gmail.com>
2018-08-01 12:24:24 -0700

    js radixSort!

---
+++ radixSort.js
@@ -0,0 +1,48 @@
+var data = data = [0, 5, 100, 1000, 75, 3, 44, 3490, 23, 4, 435, 123, 49, 403, 2498]
+
+function getDigit(num, place, base = 10) {
+	let value = 0
+	while (place) {
+		value = num % base
+		num = Math.floor((num - value) / base)
+		place--
+	}
+	return value
+}
+
+function radixSort(arr, base = 10) {
+	let maxDigits = Math.floor(Math.log10(Math.max(...arr))) + 1
+
+	for ( let num = 0; num < maxDigits; num++) {
+		// make buckets
+		let buckets = [];
+		let index = 0
+
+		for (let bucket = 0; bucket < base; bucket++) {
+			buckets.push([])
+		}
+		// iterate over array
+		arr.forEach( number => {
+			// get digit in the 1s,10s,100s, etc place
+			let digit = getDigit(number, num + 1)
+			// push number into appropriate bucket
+			buckets[digit].push(number)
+		})
+		console.log(buckets);
+
+		// ternary to push non-empty buckets back into arr
+		buckets.forEach((bucket) => {
+			bucket ? bucket.forEach( value => {
+				arr[index] = value
+				index++
+			}) : null
+		})
+	}
+
+	return arr
+}
+
+// let digit = getDigit(1234, 7)
+// console.log(digit)
+
+console.log(radixSort(data));
