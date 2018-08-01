fc111f8ea98c96fd3e532ccc447875da90a12883
shullmb <shullmb@gmail.com>
2018-08-01 09:51:47 -0700

    radix sort py solution

--- radix_sort.py
+++ radix_sort.py
@@ -1,34 +1,37 @@
 from random import shuffle
+import math
 
-data = [0,5,-3,100,1000,75,3,44,3490,23,4,435,123,49,403,-30,2498]
+data = [0,5,100,1000,75,3,44,3490,23,4,435,123,49,403,2498]
 
-def radix_sort(array, base=10):
-    def list_to_buckets(array, base, iteration):
-        buckets = [[] for x in range(base)]
-        for number in array:
-            # Isolate the base-digit from the number
-            digit = (number // (base ** iteration)) % base
-            # Drop the number into the correct bucket
-            buckets[digit].append(number)
-        return buckets
+def get_digit(num, place, base=10):
+    value = 0
+    while place > 0:
+        value = num % base
+        num = (num - value) // base
+        place -= 1
+    return value
 
-    def buckets_to_list(buckets):
-        numbers = []
-        for bucket in buckets:
-            # append the numbers in a bucket
-            # sequentially to the returned array
-            for number in bucket:
-                numbers.append(number)
-        return numbers
+def radix_sort(arr, base=10):
+    max_digits = math.trunc(math.log(max(arr), base)) + 1
+    buckets = None
+    index = 0
 
-    maxval = max(array)
-
-    it = 0
-    # Iterate, sorting the array by each base-digit
-    while base ** it <= maxval:
-        array = buckets_to_list(list_to_buckets(array, base, it))
-        it += 1
-
-    return array
-
+    for i in range(max_digits):
+        buckets = [[] for bucket in range(base)]
+        for j in range((len(arr) - 1)):
+            digit = get_digit(arr[j], i + 1)
+            buckets[digit].append(arr[j])
+            print(buckets)
+        
+        index = 0
+        for n in range(len(buckets) - 1):
+            if buckets[n] and len(buckets[n]) > 0:
+                for j in range((len(buckets[n]) - 1)):
+                    arr[index] = buckets[n][j]
+                    index += 1
+                    print('Sorting',arr)
+    return arr
+            
+# print(get_digit(-123,2))
+# print(len(data))
 print(radix_sort(data))
