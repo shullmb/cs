8f37f146acbb058dddd70c2d51fe891ab0278b35
shullmb <shullmb@gmail.com>
2018-08-22 10:22:15 -0700

    ruby linked list

--- linked_list.rb
+++ linked_list.rb
@@ -1,12 +1,14 @@
 class Node
-  def initialize(data, next_node)
+  attr_accessor(:data, :next_node)
+  def initialize(data)
     @data = data
-    @next_node = next_node
+    @next_node = nil
   end
 end
 
 class LinkedList
-  def initialize(head)
+  attr_accessor(:head)
+  def initialize
     @head = nil
   end
 
@@ -16,17 +18,51 @@ class LinkedList
       @head = node
     else
       current = @head
-      while current != nil do
+      while current != nil
         current = current.next_node
       end
       current.next_node = node
     end
   end
 
-  def delete(data)
+  def delete_where_data_equals(data)
     if @head == nil
       return false
-    
+    end
+    current = @head
+    if current.data == data
+      temp_node = current.next_node
+      current.next_node = nil
+      @head = temp_node
+    else
+      while current.next_node != nil
+        previous = current
+        current = current.next_node
+        if current.data == data
+          temp_node = current.next_node
+          previous.next_node = temp_node
+          current.next_node = nil
+          return true
+        end
+      end
+    end
+    return false
+  end
+
+  def print_list
+    if @head != nil
+      current = @head
+      puts @head.data
+      while current.next_node != nil
+        current = current.next_node
+        puts current.data
+      end
+    end
   end
 
 end
+
+list = LinkedList.new
+
+list.add("one")
+list.print_list();
